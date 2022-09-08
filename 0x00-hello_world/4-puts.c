113e7edb7b397da8e8777cfb6a91a901028c43ab
Afelloh <felixotieno540@gmail.com>
2022-09-08 05:36:56 -0700

    my file put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- * * main - prints out a string via puts
- *
- * * Description: Program that prints out the string s followed by a new line
- *
- * * Return: int 0 when done and no errors
- *
- * */
-
-
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
