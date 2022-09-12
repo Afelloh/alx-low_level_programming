22a0b7e616ffac9aa19bc593f665db4945f94f0f
Afelloh <felixotieno540@gmail.com>
2022-09-12 10:34:04 -0700

    my file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  *main - Prints all alphabets in lowercase in revers order
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	char l_letter = 122
+
+		while (l_letter >= 97)
+		{
+			putchar(l_letter);
+			l_letter--;
+		}
+
+	putchar('\n');
+
+	return (0)
+}
