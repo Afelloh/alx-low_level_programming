55f59e5a293d0236daa7a0c15496b53756110e01
Afelloh <felixotieno540@gmail.com>
2022-09-12 07:42:35 -0700

    my file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lowercase
+ * Return: 0
+ * */
+
+int main(void)
+{
+	char 1_letter;
+
+	for(1_letter = 97; 1_letter <= 122; 1_letter++)
+	{
+		putchar(1_letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
