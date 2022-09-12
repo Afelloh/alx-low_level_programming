22ccad2432d1be807f3731196233e35b7bd941d8
Afelloh <felixotieno540@gmail.com>
2022-09-12 10:24:50 -0700

    my file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - Prints all number of base 10, without using type char
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0)
+}
