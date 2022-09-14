18cddca205057973ca5830a958602478fe3184f7
Afelloh <felixotieno540@gmail.com>
2022-09-14 00:39:58 -0700

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main"
+#include <stdio.h>
+/**
+  *add - Add two integers
+  *@x: int tpe number
+  *@y: int type number
+  *Return: result of addition
+  */
+int add(int x, int y)
+{
+	return (x + y);
+}
