2f490c902109f0990f61cbbc865b2a81dd1793eb
Afelloh <felixotieno540@gmail.com>
2022-09-15 08:43:43 -0700

    main file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef main.h
+#define main.h
+
+#include <stdio.h>
+
+void positive_or_nmegative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_days(int month, int day);
+
+#endif /* main.h */
