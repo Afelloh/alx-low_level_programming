b8fb91c161ddb220ba28b8ff4bdb225e91a0fd57
Afelloh <felixotieno540@gmail.com>
2022-09-22 07:41:27 -0700

    updated function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,28 @@
 #include "main.h"
-/**
-  *_memset - Entry point
-  *@s: pointed destination
-  *@b: constant byte
-  *@n: bytes
-  *Return: Always 0 (Success)
-  */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
+#include <stdio.h>
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+/**
+  *_strcat - Concatenates two strings
+  *@dast: The destination string
+  *@src: The source string
+  *
+  *Return: A pointer to the resulting string dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
 }
