e0139985ac631285f3cf389055b98a16e52e0c1a
Afelloh <felixotieno540@gmail.com>
2022-09-22 07:11:42 -0700

    updated main file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
-nt _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -7,4 +10,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+void set_string(char **s, char *to)
+
+#endif /* HOLBERTON_H */
