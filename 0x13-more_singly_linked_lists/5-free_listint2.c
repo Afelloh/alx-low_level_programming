997356ef81a2716435560b4d78d0afe93e7dcd8b
Afelloh <felixotieno540@gmail.com>
2022-10-27 21:53:00 -0700

    program that frees others

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,53 +1,30 @@
- #include <stdlib.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * free_listint2 - ...
  * @head: ...
  *
-
-	  * Return: ...
-
-	    */
-
+ * Return: ...
+ */
 void free_listint2(listint_t **head)
-
 {
+	listint_t *temp;
 
-		listint_t *temp;
+	if (head)
+	{
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+	}
+	else
+	{
+		return;
+	}
 
-
-
-			if (head)
-
-					{
-
-								while (*head)
-
-											{
-
-															temp = (*head);
-
-																		*head = (*head)->next;
-
-																					free(temp);
-
-																							}
-
-									}
-
-				else
-
-						{
-
-									return;
-
-										}
-
-
-
-					free(*head);
-
-						head = 0;
-
+	free(*head);
+	head = 0;
 }
